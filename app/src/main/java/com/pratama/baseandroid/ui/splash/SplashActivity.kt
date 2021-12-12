81ef37a5746d1ba65670da428608ddc807a628e6
Milad Mosavi <milad.mosavi.developer@gmail.com>
2021-12-12 17:32:19 +0330

    feat(splash): add splash screen
    issue #6

--- app/src/main/java/com/pratama/baseandroid/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/pratama/baseandroid/ui/splash/SplashActivity.kt
@@ -3,14 +3,40 @@ package com.pratama.baseandroid.ui.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.testing.launchFragment
 import com.pratama.baseandroid.R
+import com.pratama.baseandroid.coreandroid.base.BaseActivityBinding
+import com.pratama.baseandroid.databinding.ActivityHomeBinding
+import com.pratama.baseandroid.databinding.ActivitySplashBinding
 import com.pratama.baseandroid.ui.homepage.HomePageActivity
+import kotlinx.coroutines.*
+import render.animations.Attention
+import render.animations.Bounce
+import render.animations.Fade
+import render.animations.Render
 
-class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+class SplashActivity : BaseActivityBinding<ActivitySplashBinding>() {
 
-        startActivity(Intent(this, HomePageActivity::class.java))
+    override val bindingInflater: (LayoutInflater) -> ActivitySplashBinding
+        get() = ActivitySplashBinding::inflate
+
+    override fun setupView(binding: ActivitySplashBinding) {
+        GlobalScope.launch {
+            withContext(Dispatchers.Main){
+                animateViews(binding)
+            }
+            delay(3000)
+            startActivity(Intent(this@SplashActivity, HomePageActivity::class.java))
+            finish()
+        }
+    }
+
+    fun animateViews(binding: ActivitySplashBinding) {
+        val render = Render(this)
+        // Set Animation
+        render.setAnimation(Fade().In(binding.imageView))
+        render.setAnimation(Fade().In(binding.imageView2))
+        render.start()
     }
 }
